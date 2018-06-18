e8c25a81fb60f763d7d822ed621e69cf1965c5a2
Jernej Skrabec <jernej.skrabec@siol.net>
2018-06-18 21:33:05 +0200

    gpu-sunxi: Update to r8p1

--- packages/linux-drivers/gpu-sunxi/package.mk
+++ packages/linux-drivers/gpu-sunxi/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of LibreELEC - https://LibreELEC.tv
-#      Copyright (C) 2016 Team LibreELEC
+#      Copyright (C) 2016-present Team LibreELEC
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="gpu-sunxi"
-PKG_VERSION="r6p2-01rel0"
-PKG_SHA256="bb49d23ab3d9fbeb701a127e6f28cff1c963bba05786f98d76edff1df0fe6c52"
+PKG_VERSION="r8p1-00rel0"
+PKG_SHA256="0036796165612eea75bee0e756fbd50c3137be257d4739b91aad0c159ae4016a"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://developer.arm.com/products/software/mali-drivers/utgard-kernel"
@@ -32,27 +32,16 @@ PKG_LONGDESC="gpu-sunxi: Linux drivers for Mali GPUs found in Allwinner SoCs"
 PKG_TOOLCHAIN="manual"
 PKG_IS_KERNEL_PKG="yes"
 
-DRIVER_DIR=$PKG_BUILD/src/devicedrv/mali/
-
-pre_patch() {
-  # move source dir to allow patching
-  mv $PKG_BUILD/driver/src $PKG_BUILD/
-  rm -rf $PKG_BUILD/driver
-}
+PKG_DRIVER_DIR=$PKG_BUILD/driver/src/devicedrv/mali/
 
 make_target() {
-  USING_UMP=0 \
-  BUILD=$BUILD \
-  USING_PROFILING=0 \
-  MALI_PLATFORM=sunxi \
-  USING_DVFS=1 \
-  USING_DEVFREQ=0 \
-  KDIR=$(kernel_path) \
-    kernel_make -C $DRIVER_DIR
+  kernel_make -C $(kernel_path) M=$PKG_DRIVER_DIR MALI_PLATFORM_FILES=platform/sunxi/sunxi.c \
+    EXTRA_CFLAGS="-DCONFIG_MALI_DVFS -DMALI_FAKE_PLATFORM_DEVICE=1 -DCONFIG_MALI_DMA_BUF_MAP_ON_ATTACH" \
+    CONFIG_MALI400=m CONFIG_MALI_DVFS=y CONFIG_MALI400_DEBUG=y
 }
 
 makeinstall_target() {
-  kernel_make -C $(kernel_path) M=$DRIVER_DIR \
+  kernel_make -C $(kernel_path) M=$PKG_DRIVER_DIR \
     INSTALL_MOD_PATH=$INSTALL/$(get_kernel_overlay_dir) INSTALL_MOD_STRIP=1 DEPMOD=: \
     modules_install
 }
