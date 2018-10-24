37b899d401bada05950f1ba213201b9e216dcc7c
sky42 <sky2t3t7@gmail.com>
2018-10-24 21:33:20 +0200

    kernel-firmware: update from LE 9.0 alpha 006

--- packages/linux-firmware/kernel-firmware/package.mk
+++ packages/linux-firmware/kernel-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kernel-firmware"
-PKG_VERSION="8d69bab"
+PKG_VERSION="fea76a04f25fd0a217c0d566ff5ff8f23ad3e648"
 PKG_ARCH="any"
 PKG_LICENSE="other"
 PKG_SITE="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/"
