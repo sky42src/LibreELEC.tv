17e19704d5a28d6da4ad34768e115123e9b4e8c0
sky42 <sky2t3t7@gmail.com>
2018-10-27 07:13:43 +0200

    iwlwifi-firmware: update to iwlwifi-firmware-bc32183

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="e8e1c45"
+PKG_VERSION="bc32183c521559114eb72c0677b850269772addb"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
