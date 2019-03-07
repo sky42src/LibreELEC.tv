a1ef99792424fa0496f5907bbcc53e617f5e6aac
sky42 <sky2t3t7@gmail.com>
2019-03-07 18:15:59 +0000

    device-trees-amlogic: add Vero 4K(+) dtb files from OSMC
    taken from git checkout OSMC kernel 3.14.29-139 and changed a bit,
    so that the LE update scripts also finds the vero dtb files

--- projects/Amlogic/packages/device-trees-amlogic/package.mk
+++ projects/Amlogic/packages/device-trees-amlogic/package.mk
@@ -20,6 +20,7 @@ make_target() {
                 gxbb_p201 gxbb_p200_1G_wetek_hub gxbb_p200_2G_wetek_play_2 \
                 gxl_p212_1g gxl_p212_2g gxl_p281_1g gxl_p212_1g_lepotato gxl_p212_2g_lepotato \
                 gxm_q200_2g gxm_q201_1g gxm_q201_2g \
+                gxl_p212_vero4k gxl_p231_vero4kplus \
 	      )
 
   # Add trees to the list
