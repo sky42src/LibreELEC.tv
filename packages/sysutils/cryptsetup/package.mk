cb7d6ac103d138916498bd4cc79343cd65e814a7
sky42 <sky2t3t7@gmail.com>
2019-06-14 15:13:14 +0000

    cryptsetup: cryptsetup-2.0.6

---
+++ packages/sysutils/cryptsetup/package.mk
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+# dont update, the old version is intentional
+#
+PKG_NAME="cryptsetup"
+PKG_VERSION="2.0.6"
+PKG_SHA256="7c51fae0f0e7ea9af0f515b2ac77009fb2969a6619ebab47d097dca38b083d30"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://gitlab.com/cryptsetup/cryptsetup"
+PKG_URL="https://www.kernel.org/pub/linux/utils/cryptsetup/v2.0/cryptsetup-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain libgcrypt popt json-c util-linux lvm2"
+PKG_SECTION="sysutils"
+PKG_SHORTDESC="Userspace setup tool for transparent encryption of block devices using dm-crypt"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+CRYPTSETUP_CONFIGURE_OPTS="--enable-fips \
+                           --disable-veritysetup \
+                           --disable-cryptsetup-reencrypt \
+                           --disable-integritysetup \
+                           --enable-gcrypt-pbkdf2 \
+                           --with-sysroot=$SYSROOT_PREFIX \
+                           --with-libgcrypt-prefix=$SYSROOT_PREFIX/usr"
+
+PKG_CONFIGURE_OPTS_TARGET="$CRYPTSETUP_CONFIGURE_OPTS"
