6274b73522072e8b55777d587977cad851a86323
sky42 <sky2t3t7@gmail.com>
2019-06-18 16:19:00 +0000

    openssl: openssl-1.0.2s

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="openssl"
-PKG_VERSION="1.0.2r"
+PKG_VERSION="1.0.2s"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
