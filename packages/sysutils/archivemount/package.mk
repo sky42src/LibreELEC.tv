62866bc05a580f5e33c52eda3162aeb40167714d
sky42 <sky2t3t7@gmail.com>
2019-10-02 12:40:29 +0200

    archivemount: for mounting archives without extract

---
+++ packages/sysutils/archivemount/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="archivemount"
+PKG_VERSION="104a314849ff8a3793f6cd7c4c9e2940b5c0d513"
+PKG_SHA256="5693f35bab9361ffa880bbd5091fe939914091c8bc5d4274e3113bacb491f828"
+PKG_LICENSE="LGPL"
+PKG_SITE="https://github.com/cybernoid/archivemount"
+PKG_URL="https://github.com/cybernoid/archivemount/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="fuse libarchive"
+PKG_LONGDESC="archivemount: mounting archives without extracting"
+PKG_TOOLCHAIN="autotools"
+
