ed521b8119c45fa8f9e19ded83cb9750d34ec22d
sky42 <sky2t3t7@gmail.com>
2022-06-04 21:12:20 +0200

    sky42 tool rsync parts from master

---
+++ tools/RPi/rsync-from-master-515.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in packages/linux/patches/raspberrypi packages/tools/rpi-eeprom packages/graphics/bcm2835-driver packages/tools/bcm2835-bootloader projects/RPi/devices/RPi2/linux projects/RPi/devices/RPi4/linux ; do
+  rsync -SrtlvHx --delete ../../master/${i}/ ${i}
+done
