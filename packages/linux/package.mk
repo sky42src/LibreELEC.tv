6a0486c3948ac926cee3e9660357c2db31260109
sky42 <sky2t3t7@gmail.com>
2022-07-13 16:42:40 +0200

    linux: update linux-5.10.130

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -28,8 +28,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.10.123"
-    PKG_SHA256="654ab0960b70013e7dad6b3782c25d62e13cbb8c053010daef667d5d74061e52"
+    PKG_VERSION="5.10.130"
+    PKG_SHA256="e144a6d851a8e2977743d45fdb9cdb1b7f4aabde09533e73c969cded4daa962c"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
