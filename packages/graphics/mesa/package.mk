934ef20744e558bb7d4d835ec8163711e6b5aa05
sky42 <sky2t3t7@gmail.com>
2022-08-12 20:51:10 +0200

    mesa: update to 21.3.9

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="21.0.3"
-PKG_SHA256="565c6f4bd2d5747b919454fc1d439963024fc78ca56fd05158c3b2cde2f6912b"
+PKG_VERSION="21.3.9"
+PKG_SHA256="91fe6f20339884e37a7c47bfba42fb1cd397512c9ed5ad423de369f047cd8b5c"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
