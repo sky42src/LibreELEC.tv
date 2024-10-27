4499af58a754b5f7421d0af5d4c0f1707f162c78
Peter Tuschy <sky42+git@mailbox.org>
2024-10-27 19:41:57 +0100

    gmmlib: update to 22.5.2

--- packages/multimedia/gmmlib/package.mk
+++ packages/multimedia/gmmlib/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gmmlib"
-PKG_VERSION="22.3.18"
-PKG_SHA256="381f7ad104e3d0d8dd6c3ec0c8304e410a479c439e72db35c83fb7e2b0ec556f"
+PKG_VERSION="22.5.2"
+PKG_SHA256="dbf7cc401de7ff386306a23c1c61b3cf7bd86a4d9001b3a1d16a81e6b0e3ab2b"
 PKG_ARCH="x86_64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://01.org/linuxmedia"
