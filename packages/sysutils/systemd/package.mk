be29869550358b0bfc3cb167eb97f73abd91c9a9
Peter Tuschy <sky42+git@mailbox.org>
2024-10-27 14:43:54 +0100

    systemd: update to 255.10

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="systemd"
-PKG_VERSION="255.4"
-PKG_SHA256="96e75bd08c57ad401677456fb88ef54a9f05bb1695693013bc6ecce839640fd5"
+PKG_VERSION="255.10"
+PKG_SHA256="1747b848e68223597abb90ca2758b25230ac4c19e252e9ec77c8518750f621ed"
 PKG_LICENSE="LGPL2.1+"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/systemd"
 PKG_URL="https://github.com/systemd/systemd-stable/archive/v${PKG_VERSION}.tar.gz"
