3c5300db2132fde213391c01348cab985f89c121
Peter Tuschy <sky42+git@mailbox.org>
2024-10-29 22:33:36 +0100

    linux: update to 6.11.5

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,8 +29,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="6.6.57"
-    PKG_SHA256="66ce426ef96f99b8e1ef7ac72e780c730ef8b970f7aa5708501c4274d7abb7b3"
+    PKG_VERSION="6.11.5"
+    PKG_SHA256="471485b3b7f2fb637bd8fe3d00944c4c135c7d8ee02f357f33690baab0752a07"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
