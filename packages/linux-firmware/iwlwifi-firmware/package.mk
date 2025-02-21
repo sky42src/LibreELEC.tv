6668375f2077a090ff4b4db520139861d9143b92
Peter Tuschy <sky42+git@mailbox.org>
2025-02-21 17:26:17 +0100

    iwlwifi-firmware: update to fb888a7

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="8f2ecadb578d4ceb27cd2b596b1d0d646ba63d2d"
-PKG_SHA256="e215b997b3dc5a3fad0072133941e3067f0b602b1902345fe298bb119dbda1d7"
+PKG_VERSION="fb888a7b059cc228741875fb48b97f9c3ed429fd"
+PKG_SHA256="c6f6ac38da6e364c5076cbda168e602a355c58a91c9f59eee4b7a9de7d3e8ae5"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
 PKG_URL="https://github.com/LibreELEC/iwlwifi-firmware/archive/${PKG_VERSION}.tar.gz"
